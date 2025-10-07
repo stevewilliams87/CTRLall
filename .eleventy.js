2cc1c84215d7b1d71ead9af0b1ab2840d26e63da
stevewilliams87@outlook.com.au <stevewilliams87@outlook.com.au>
2025-10-07 22:14:34 +1100

    Deploy Eleventy build to /docs for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -16,7 +16,7 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: "src",
       includes: "../_includes",
-      output: "_site"
+      output: "docs"
     },
     pathPrefix: "/",
     htmlTemplateEngine: "liquid",
